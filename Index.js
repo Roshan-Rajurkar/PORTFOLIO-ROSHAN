27309992f21490de7447b1e0d8a0153461d9df86
Roshan-Rajurkar <roshangit50@gmail.com>
2023-02-24 21:42:15 +0530

    footer and contact page

--- Index.js
+++ Index.js
@@ -15,4 +15,28 @@ function changeMode() {
   toggle.style.transition = "all .6s ease-in";
 }
 
+// email using email.js
+function sendMail() {
+  var params = {
+    name: document.getElementById("myname").value,
+    email: document.getElementById("myemail").value,
+    message: document.getElementById("mymessage").value,
+  };
 
+  const serviceId = "service_ubng6e9";
+  const templateId = "template_nzi7psu";
+
+  // sending email
+  emailjs
+    .send(serviceId, templateId, params)
+    .then((res) => {
+      document.getElementById("myname").value = "";
+      document.getElementById("myemail").value = "";
+      document.getElementById("mymessage").value = "";
+      console.log(res);
+      alert("Your message sent successfully.");
+    })
+    .catch((err) => {
+      console.log("Check msg");
+    });
+}
